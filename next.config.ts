7c7f05086ccb28840e1366efafacb9f34fba4693
阿呆 <dzy95279651@163.com>
2026-01-27 23:11:27 +0800

    Initial commit: Next.js demo project

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "www.baidu.com",
+      },
+    ]
+  },
 };
 
 export default nextConfig;
---
+++ src/app/components/MyButton.tsx
@@ -0,0 +1,5 @@
+export default function MyButton({children}: { children: React.ReactNode }) {
+  return (
+    <button className="bg-blue-500 text-white px-4 py-2 rounded-md">{children}</button>
+  );
+}
